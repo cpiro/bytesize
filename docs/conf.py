db43343a2a962dc5ee3186e035c652441ecc95fc
Chris Piro <cpiro@cpiro.com>
2015-10-28 16:22:56 -0400

    cp

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ author = 'Chris Piro'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.1'  # xxx generate from setup.py
 # The full version, including alpha/beta/rc tags.
 release = '0.1'
 
@@ -117,7 +117,9 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'classic'
+#html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
